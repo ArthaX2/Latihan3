d9720a0940f7fc0fe56a8dabc9a2088f3ebeaa1e
ArthaX2 <139363030+ArthaX2@users.noreply.github.com>
2024-02-21 15:51:05 +0700

    Add files via upload

---
+++ lat1.py
@@ -0,0 +1,14 @@
+notes = ["Do", "Re", "Mi", "Fa", "So", "La", "Ti"]
+
+
+mi = notes[2:3]
+
+do = notes[:1]
+
+rest = notes[3:]
+
+re = notes[1:2]
+
+new_order = mi + do + rest + re
+
+print(new_order)
